fe7c1aba5c63daa2a4a2edc0728249e15fa3b450
Vladimir <gvadel0106@gamil.com>
2023-12-01 19:12:09 +0300

    comit lets do it

---
+++ ProjectX/ProjectX/ProjectXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectXApp.swift
+//  ProjectX
+//
+//  Created by Vladimir Grishchenkov on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
